82fd6fc496f8059c69d85aa91e0d86921d2f32ea
Olivier Bonnemaison <bonnemai@yahoo.fr>
2025-09-28 18:58:20 +0100

    Test locally.

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 set -euo pipefail
 
 NAME=custom-basket-ui
-VITE_CUSTOM_BASKET_API_URL=${VITE_CUSTOM_BASKET_API_URL:-https://custom-basket-api.20urgjtsc9b0.eu-gb.codeengine.appdomain.cloud/}
+# VITE_CUSTOM_BASKET_API_URL=${VITE_CUSTOM_BASKET_API_URL:-https://custom-basket-api.20urgjtsc9b0.eu-gb.codeengine.appdomain.cloud/}
+VITE_CUSTOM_BASKET_API_URL=${VITE_CUSTOM_BASKET_API_URL:-http://localhost:8000/}
+
 
 docker stop "$NAME" >/dev/null 2>&1 || true
 docker rm "$NAME" >/dev/null 2>&1 || true
